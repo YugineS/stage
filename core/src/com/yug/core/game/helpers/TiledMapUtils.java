ad7007923da721c59aec11793d1fa8108e19cd42
Yauhen Sakhin <yuginesakhin@gmail.com>
2015-02-13 02:14:36 +0300

    adding TiledMapUtils

---
+++ core/src/com/yug/core/game/helpers/TiledMapUtils.java
@@ -0,0 +1,24 @@
+package com.yug.core.game.helpers;
+
+import com.badlogic.gdx.maps.MapProperties;
+
+/**
+ * Created by yugine on 12.2.15.
+ */
+public class TiledMapUtils
+{
+    public static String getStringPropery(final String key, final MapProperties properties)
+    {
+        return (String)properties.get(key);
+    }
+
+    public static boolean getBooleanProperty(final String key, final MapProperties properties)
+    {
+        return Boolean.TRUE.toString().equalsIgnoreCase(getStringPropery(key, properties));
+    }
+
+    public static int getIntProperty(final String key, final MapProperties properties)
+    {
+        return Integer.valueOf(getStringPropery(key, properties)).intValue();
+    }
+}
